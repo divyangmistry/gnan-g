2920ed088a3c575032e8cca943c5375b44f9a660
Milan Vadher <milanvadher1996@gmail.com>
2019-01-22 21:30:47 +0530

    Add Forgot password page ...

--- lib/UI/auth/forgot_password.dart
+++ lib/UI/auth/forgot_password.dart
@@ -1,6 +1,7 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import '../../Service/apiservice.dart';
+import '../../colors.dart';
+import '../../common.dart';
 
 class ForgotPassword extends StatefulWidget {
   @override
@@ -10,232 +11,97 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class ForgotPasswordState extends State<ForgotPassword> {
-  _ForgotPasswordData _data = new _ForgotPasswordData();
   final _formKey = GlobalKey<FormState>();
   ApiService _api = new ApiService();
   bool _autoValidate = false;
+  String _mhtId;
 
-  final _passwordController = new TextEditingController();
-
-  _appBarView() {
-    return new AppBar(
-      centerTitle: true,
-      title: new Text(
-        'FORGOT PASSWORD',
-        style: TextStyle(
-            color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w300),
-      ),
-    );
-  }
-
-  _bodyView() {
+  @override
+  Widget build(BuildContext context) {
     return new Form(
       key: _formKey,
       autovalidate: _autoValidate,
-      child: new Container(
-        child: new ListView(
-          padding: new EdgeInsets.all(20.0),
-          children: <Widget>[
-            new Image.asset(
-              'images/planet.png',
-              height: 200.0,
-            ),
-            new Padding(
-              padding: EdgeInsets.only(top: 60.0),
-            ),
-            new TextFormField(
-              controller: _passwordController,
-              validator: (value) {
-                Pattern pattern =
-                    r'(?=^.{6,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$';
-                RegExp regex = new RegExp(pattern);
-                if (value.isEmpty) {
-                  return 'Password is required';
-                } else if (value.length < 6) {
-                  return 'Passwords must contain at least six characters';
-                } else if (!regex.hasMatch(value)) {
-                  return 'Passwords must contain uppercase, lowercase letters and numbers';
-                }
-              },
-              onSaved: (String value) {
-                this._data.password = value;
-              },
-              obscureText: true,
-              decoration: new InputDecoration(
-                  labelText: 'New Password',
-                  hintText: 'Enter your new password',
-                  suffixIcon: new Icon(Icons.lock_open),
-                  border: OutlineInputBorder()),
-            ),
-            new Padding(
-              padding: EdgeInsets.only(top: 20.0),
-            ),
-            new TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Password is required';
-                } else if (value != _passwordController.text) {
-                  return 'Password & Verify Password must be same';
-                }
-              },
-              onSaved: (String value) {
-                this._data.retypePassword = value;
-              },
-              obscureText: true,
-              decoration: new InputDecoration(
-                labelText: 'Verify Password',
-                hintText: 'Verify your Password',
-                suffixIcon: new Icon(Icons.lock_outline),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            new Padding(
-              padding: EdgeInsets.only(top: 20.0),
-            ),
-            new Container(
-              height: 50.0,
-              width: 15.0,
-              child: new RaisedButton(
-                shape: new RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(300.0)),
-                onPressed: _changePassword,
-                color: Theme.of(context).primaryColor,
-                child: new Text(
-                  'Reset Password',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20.0,
+      child: new Scaffold(
+        backgroundColor: kQuizSurfaceWhite,
+        body: SafeArea(
+          child: new ListView(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            children: <Widget>[
+              new SizedBox(height: 40.0),
+              new Column(
+                children: <Widget>[
+                  new Image.asset(
+                    'images/logo1.png',
+                    height: 150,
                   ),
+                  new SizedBox(height: 30.0),
+                  new Text(
+                    'FORGOT PASSWORD',
+                    textScaleFactor: 1.5,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ],
+              ),
+              new SizedBox(
+                height: 50.0,
+              ),
+              new AccentColorOverride(
+                color: kQuizBrown900,
+                child: new TextFormField(
+                  validator: _mhtIdValidation,
+                  decoration: InputDecoration(
+                    labelText: 'Mht Id',
+                    hintText: 'Enter Mht Id no.',
+                    prefixIcon: Icon(
+                      Icons.person_outline,
+                      color: kQuizBrown900,
+                    ),
+                    filled: true,
+                  ),
+                  onSaved: (String value) {
+                    _mhtId = value;
+                  },
+                  keyboardType: TextInputType.numberWithOptions(),
                 ),
               ),
-            ),
-          ],
+              new SizedBox(height: 20.0),
+              new RaisedButton(
+                child: Text(
+                  'Send OTP',
+                  style: TextStyle(color: Colors.white),
+                ),
+                elevation: 4.0,
+                padding: EdgeInsets.all(20.0),
+                onPressed: _submit,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void _changePassword() {
+  String _mhtIdValidation(value) {
+    if (value.isEmpty) {
+      return 'Mht ID is required';
+    } else if (value.length != 6) {
+      return 'Mht ID must have 6 digit';
+    }
+    return null;
+  }
+
+  void _submit() {
     if (_formKey.currentState.validate()) {
-      _formKey.currentState.save();
-      print('FORGOT PASSWORD : ');
-      print(_data.password);
-      print(_data.retypePassword);
-      var data;
-      data = {
-        'password': _data.password,
-        'verifyPassword': _data.retypePassword
-      };
-      _api.forgotPassword(json.encode(data)).then((res) {
-        if (res.statusCode == 201) {
-          _showError('Success', json.decode(res.body)['msg'], false);
-          print(json.decode(res.body).toString());
-          Navigator.pushReplacementNamed(context, '/login');
-        } else {
-          _showError(
-              'Error',
-              json.decode(res.body)['msg'] ? json.decode(res.body)['msg'] : '',
-              false);
-        }
-      });
+      Navigator.pop(context);
+      Navigator.pushReplacementNamed(context, '/otp_new');
+      print(_mhtId);
+      // TODO : Implement Verify OTP
     } else {
       _autoValidate = true;
     }
-  }
-
-  void _showError(String title, String msg, bool showCancel) {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (_) {
-        return AlertDialog(
-          shape: new RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(7.0),
-          ),
-          title: Text(title, textAlign: TextAlign.center),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                msg,
-                style: TextStyle(color: Colors.blueGrey),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  showCancel
-                      ? FlatButton(
-                          shape: new RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5.0),
-                              side: BorderSide(color: Colors.grey)),
-                          child: Row(
-                            children: <Widget>[
-                              Icon(
-                                Icons.close,
-                                size: 22.0,
-                                color: Colors.blue[900],
-                              ),
-                              SizedBox(
-                                width: 5.0,
-                              ),
-                              Text(
-                                'Cancel',
-                                style: TextStyle(color: Colors.blue[900]),
-                              )
-                            ],
-                          ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        )
-                      : new Container(width: 0, height: 0),
-                  showCancel
-                      ? SizedBox(
-                          width: 10.0,
-                        )
-                      : new Container(width: 0, height: 0),
-                  FlatButton(
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0)),
-                    color: Colors.blue[900],
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.done,
-                          size: 22.0,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 5.0,
-                        ),
-                        Text(
-                          'Try Again',
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: _appBarView(),
-      body: _bodyView(),
-    );
+    //Navigator.pushNamed(context, '/otp_new');
   }
 }
 
