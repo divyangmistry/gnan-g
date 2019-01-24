013b6bf8895c57f18afbdbaa148094c34c8feda1
Parth Gudhka <93parth@gmail.com>
2019-01-24 17:04:31 +0300

    Add Ordinal number function
    This function will return "st", "nd", "rd" or "th" correctly according
    to the rank of user.

--- lib/UI/leaderboard.dart
+++ lib/UI/leaderboard.dart
@@ -80,7 +80,7 @@ class LeaderBoardState extends State<LeaderBoard> {
                       color: Colors.white
                     ),
                   ),
-                  Text('nd',
+                  Text(getOrdinalOfNumber(rank),
                     style: TextStyle(
                         color: Colors.white
                     ),)
@@ -133,6 +133,21 @@ class LeaderBoardState extends State<LeaderBoard> {
         ],
       ),
     );
+  }
+
+  String getOrdinalOfNumber (int n) {
+    int j = n % 10;
+    int k = n % 100;
+    if (j == 1 && k != 11) {
+      return "st";
+    }
+    if (j == 2 && k != 12) {
+      return "nd";
+    }
+    if (j == 3 && k != 13) {
+      return "rd";
+    }
+    return "th";
   }
   
   @override
