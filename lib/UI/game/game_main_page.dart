d5e711abcf2dbe523b5e3dcd2c19c2b9898cdc43
divyang7777 <divyangmistry7777@gmail.com>
2019-02-20 23:23:50 +0530

    implemented home page with animation

--- lib/UI/game/game_main_page.dart
+++ lib/UI/game/game_main_page.dart
@@ -1,7 +1,14 @@
+import 'package:GnanG/UI/game_level.dart';
 import 'package:GnanG/UI/level/levelList.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:GnanG/common.dart';
+import 'package:GnanG/UI/animation/success.dart';
+import 'package:flutter/material.dart';
+import 'package:GnanG/Service/apiservice.dart';
+import 'package:GnanG/model/cacheData.dart';
+import '../../common.dart';
+import 'package:nima/nima_actor.dart';
 
 class GameMainPage extends StatefulWidget {
   @override
@@ -12,6 +19,7 @@ class GameMainPage extends StatefulWidget {
 }
 
 class GameMainPageState extends State<GameMainPage> {
+  ApiService _api = new ApiService();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -29,6 +37,20 @@ class GameMainPageState extends State<GameMainPage> {
         SizedBox(
           height: 25.0,
         ),
+        _gameBar(),
+        SizedBox(
+          height: 20.0,
+        ),
+        Center(
+            child: Center(
+          child: Image.asset(
+            'images/logo1.png',
+            height: 70.0,
+          ),
+        )),
+        SizedBox(
+          height: 20.0,
+        ),
         GestureDetector(
           onTap: () {
             Navigator.pushNamed(context, '/level_new');
@@ -40,8 +62,8 @@ class GameMainPageState extends State<GameMainPage> {
               boxShadow: [
                 BoxShadow(
                     color: Colors.grey.shade900,
-                    blurRadius: 10.0,
-                    spreadRadius: 1.0)
+                    blurRadius: 5.0,
+                    spreadRadius: 0.5)
               ],
               borderRadius: BorderRadius.all(
                 Radius.circular(20.0),
@@ -97,14 +119,14 @@ class GameMainPageState extends State<GameMainPage> {
                 Container(
                   height: 180,
                   width: 150,
-                  child: FlareActor(
-                    'assets/animation/leaderboard.flr',
-                    animation: 'clouds',
-                  ),
+                  child: new NimaActor("assets/bonus",
+                      alignment: Alignment.center,
+                      fit: BoxFit.contain,
+                      animation: "Idle"),
                 ),
                 Container(
                   child: Text(
-                    'GO TO GAME',
+                    'Bonus',
                     style: TextStyle(fontSize: 25.0, color: Colors.white),
                   ),
                 ),
@@ -117,64 +139,96 @@ class GameMainPageState extends State<GameMainPage> {
         ),
         GestureDetector(
           onTap: () {
-            Navigator.pushNamed(context, '/profile');
+            Navigator.pushNamed(context, '/gameStart');
           },
           child: Container(
-            height: MediaQuery.of(context).size.height / 4.5,
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.blue.shade400,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.shade900,
-                    blurRadius: 10.0,
-                    spreadRadius: 1.0)
-              ],
-              borderRadius: BorderRadius.all(
-                Radius.circular(20.0),
+              height: MediaQuery.of(context).size.height / 4.5,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.blue.shade400,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey.shade900,
+                      blurRadius: 10.0,
+                      spreadRadius: 1.0)
+                ],
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
               ),
-            ),
-            child: Container(
-              alignment: Alignment(0.5, 0.0),
-              child: Text(
-                'BONUS',
-                style: TextStyle(fontSize: 25.0, color: Colors.white),
-              ),
-            ),
-          ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    height: 180,
+                    width: 150,
+                    child: FlareActor(
+                      'assets/animation/person_floating.flr',
+                      animation: 'Relaxing',
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment(0.5, 0.0),
+                    child: Text(
+                      'PROFILE',
+                      style: TextStyle(fontSize: 25.0, color: Colors.white),
+                    ),
+                  ),
+                ],
+              )),
         ),
-        SizedBox(
-          height: 15.0,
-        ),
-        GestureDetector(
-          onTap: () {
-            Navigator.pushNamed(context, '/gameOf15');
-          },
-          child: Container(
-            height: MediaQuery.of(context).size.height / 4.5,
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.green.shade400,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey.shade900,
-                    blurRadius: 10.0,
-                    spreadRadius: 1.0)
-              ],
-              borderRadius: BorderRadius.all(
-                Radius.circular(20.0),
-              ),
-            ),
-            child: Container(
-              alignment: Alignment(0.5, 0.0),
-              child: Text(
-                'PUZZLE',
-                style: TextStyle(fontSize: 25.0, color: Colors.white),
-              ),
-            ),
-          ),
-        )
       ],
+    );
+  }
+
+  Widget _gameBar() {
+    return new Row(
+      children: <Widget>[
+        _iconButton(
+          Icon(Icons.power_settings_new, color: Colors.white),
+          () {
+            _api.logout();
+            Navigator.pop(context);
+            Navigator.pushReplacementNamed(context, '/login_new');
+          },
+        ),
+        new Expanded(
+          child: Center(
+              // child: Text(
+              //   'Gnan-G',
+              //   style: TextStyle(
+              //       color: Colors.white,
+              //       fontSize: 50.0,
+              //       fontWeight: FontWeight.w600),
+              // ),
+              ),
+        ),
+        _iconButton(
+          Icon(
+            Icons.help_outline,
+            color: Colors.white,
+          ),
+          () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => new SucessAnimationPage(),
+              ),
+            );
+            // Navigator.pushNamed(context, '/rules');
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _iconButton(Icon icon, Function clickEvent) {
+    return new RaisedButton(
+      onPressed: clickEvent,
+      child: icon,
+      shape: new CircleBorder(),
+      elevation: 5.0,
+      padding: const EdgeInsets.all(10.0),
     );
   }
 }
