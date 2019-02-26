0e5f78d47e416dc17f28804c3fc18ea413a36206
Parth Gudhka <93parth@gmail.com>
2019-02-26 15:46:29 +0530

    Split answer tiles by space
    We get a list of lists for answer tiles, and accordingly we display
    the answer tiles. If answer tiles exceed rowlimit, then continue on
    next line.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:SheelQuotient/app.dart';
+import 'package:flutter/rendering.dart';
 
-void main() => runApp(QuizApp());
+void main() {
+  debugPaintSizeEnabled = false;
+  runApp(QuizApp());
+}
