7bfd569ff14575fe45e7103e343385c5d74d5261
Kamlesh <Kamlesh@DESKTOP-2K84PGB>
2019-03-03 22:27:29 +0530

    Latest change

--- lib/utils/app_setting_util.dart
+++ lib/utils/app_setting_util.dart
@@ -6,18 +6,18 @@ import 'package:flame/flame.dart';
 class AppSetting {
 
   static AudioPlayer backgroundMusic;
-  static void startBackgroundMusic() {
+  static  void startBackgroundMusic() {
     AppUtils.appSound(() async {
       if(backgroundMusic == null)
-        backgroundMusic = await Flame.audio.loop('music/background_music.mp3', volume: 0.1);
+        backgroundMusic = await Flame.audio.loop('music/background_music.mp3', volume: 0.8);
       else
         backgroundMusic.resume();
     });
   }
 
+
   static void stopBackgroundMusic() {
-
+    if(backgroundMusic != null)
       backgroundMusic.pause();
-
   }
 }
