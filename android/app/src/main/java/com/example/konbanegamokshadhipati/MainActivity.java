dd5bb6d56357b62debadd81f1a58dcf5811a092f
Milan Vadher <milanvadher1996@gmail.com>
2019-03-07 15:18:07 +0530

    change app id org.mba.gnang v1.0.1 update

--- android/app/src/main/java/com/example/konbanegamokshadhipati/MainActivity.java
+++ android/app/src/main/java/com/example/konbanegamokshadhipati/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mba.sheelquotient;
+package org.mba.gnang;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
--- lib/constans/appconstant.dart
+++ lib/constans/appconstant.dart
@@ -1,6 +1,6 @@
 class AppConstant {
   //static final String APP_VERSION = "1.0.0";
-  //static final String APP_ID = "com.mba.sheelquotient";
+  //static final String APP_ID = "org.mba.gnang";
   static final String MBA_MAILID = "mbaapps@googlegroups.com";
   //static final String PLAYSTORE_URL = "https://gnang.page.link/app";
   static final String BASE_PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=";
